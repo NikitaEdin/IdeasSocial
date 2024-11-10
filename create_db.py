7caea679a444503d98296e0a07d23304676a5045
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2024-11-10 16:43:15 +0000

    Separated env creation for manual execution

--- create_db.py
+++ create_db.py
@@ -6,16 +6,23 @@ from app import app, db
 env_path = '.env'
 
 # Auto generate .ENV file with secret_key
-if not os.path.exists(env_path):
-    with open(env_path, 'w') as env_file:
-        secret_key = secrets.token_hex(16)
-        env_file.write(f"SECRET_KEY={secret_key}\n")  
-        env_file.write(f"DATABASE_URL=sqlite:///site.db\n") 
-    print('New .env file created with generated SECRET_KEY')
+def create_env_file():
+    if not os.path.exists(env_path):
+        with open(env_path, 'w') as env_file:
+            secret_key = secrets.token_hex(16)
+            env_file.write(f"SECRET_KEY={secret_key}\n")
+            env_file.write(f"DATABASE_URL=sqlite:///site.db\n")
+        print('New .env file created with generated SECRET_KEY')
+    else:
+        print('.env file already exists.')
 
 # Load environment variables
 load_dotenv(env_path)
 
+# Load environment variables
+def load_environment():
+    load_dotenv(env_path)
+    print('Environment variables loaded from .env')
 
 def create_database():
     with app.app_context():
